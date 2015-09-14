f7243a5799f6a9e89d588311448d762daca0f9cd
Henrique Silva <henrique.silva@lnls.br>
2015-09-14 11:05:13 -0300

    Change color scheme

--- init.el
+++ init.el
@@ -4,7 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(frame-background-mode (quote dark))
+;; '(frame-background-mode (quote dark))
+ '(custom-enabled-themes (quote (deeper-blue)))
  '(inhibit-startup-screen t)
  '(package-archives
    (quote
@@ -16,7 +17,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray15" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
+;; '(default ((t (:inherit nil :stipple nil :background "gray15" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "DeepSkyBlue1"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "goldenrod1"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "aquamarine1"))))
